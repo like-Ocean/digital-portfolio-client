4e4d7a85a97ffd9a2258c8a871e537a43bdfc120
like-Ocean <my.program.is.false.2p1@gmail.com>
2024-04-05 21:11:43 +0500

    user profile

---
+++ src/components/ui/UserInfo/index.jsx
@@ -0,0 +1,61 @@
+import { Avatar, Badge, Button, Card, Flex, Text } from '@mantine/core';
+import { useSelector } from 'react-redux';
+
+export const UserInfo = () => {
+    const { user } = useSelector((state) => state.user);
+    return (
+        <Card shadow="xl" padding="md">
+            <Flex justify="center" align="center" mt="md" mb="xs">
+                <Avatar color="cyan" size={90}>
+                    {user.first_name[0] + user.surname[0]}
+                </Avatar>
+            </Flex>
+            <Text align="center" mt="xs" mb="xs" fw={500}>
+                {user.login}
+            </Text>
+            <Flex gap="md" justify="center" align="flex-start" direction="column">
+                <Text size="sm">
+                    <Badge color="blue" mr="md">
+                        Имя:
+                    </Badge>
+                    {user.first_name}
+                </Text>
+
+                <Text size="sm">
+                    <Badge color="blue" mr="md">
+                        Фамилия:
+                    </Badge>
+                    {user.surname}
+                </Text>
+
+                <Text size="sm">
+                    <Badge color="blue" mr="md">
+                        Телефон:
+                    </Badge>
+                    {user.phone}
+                </Text>
+
+                <Text size="sm">
+                    <Badge color="blue" mr="md">
+                        Почта:
+                    </Badge>
+                    {user.email}
+                </Text>
+
+                <Text size="sm">
+                    <Badge color="blue" mr="md">
+                        О себе:
+                    </Badge>
+                    {user.about}
+                </Text>
+            </Flex>
+
+            <Button fullWidth mt="md" radius="md">
+                Изменить
+            </Button>
+            <Button color="red" fullWidth mt="xs" radius="md">
+                Удалить
+            </Button>
+        </Card>
+    );
+};
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,86 +1,30 @@
 import { Layout } from '../components/ui/Layout/index.jsx';
-import {
-    Card,
-    Text,
-    Button,
-    Flex,
-    Grid,
-    Avatar,
-    Badge,
-    Tabs,
-} from '@mantine/core';
+import { Grid, Tabs } from '@mantine/core';
+import { UserInfo } from '../components/ui/UserInfo/index.jsx';
 
 export const UserProfile = () => {
     return (
         <Layout>
             <Grid>
                 <Grid.Col span={3}>
-                    <Card shadow="xl" padding="md" radius="md">
-                        <Flex justify="center" align="center" mt="md" mb="xs">
-                            <Avatar
-                                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                                alt="it's me"
-                                size={100}
-                            />
-                        </Flex>
-                        <Text align="center" mt="xs" mb="xs" fw={500}>
-                            Victor Dudko
-                        </Text>
-                        <Flex gap="md" justify="center" align="flex-start" direction="column">
-                            <Text size="sm">
-                                <Badge color="blue" mr="md">
-                                    Имя:
-                                </Badge>
-                                Виктор
-                            </Text>
-
-                            <Text size="sm">
-                                <Badge color="blue" mr="md">
-                                    Фамилия:
-                                </Badge>
-                                Виктор
-                            </Text>
-
-                            <Text size="sm">
-                                <Badge color="blue" mr="md">
-                                    Телефон:
-                                </Badge>
-                                +79829282645
-                            </Text>
-
-                            <Text size="sm">
-                                <Badge color="blue" mr="md">
-                                    О себе:
-                                </Badge>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-                                provident eos fugiat id necessitatibus magni ducimus molestias.
-                                Placeat, consequatur. Quisquam, quae magnam perspiciatis excepturi
-                                iste sint itaque sunt laborum. Nihil?
-                            </Text>
-                        </Flex>
-
-                        <Button fullWidth mt="md" radius="md">
-                            Изменить
-                        </Button>
-                        <Button color="red" fullWidth mt="xs" radius="md">
-                            Удалить
-                        </Button>
-                    </Card>
+                    <UserInfo />
                 </Grid.Col>
 
                 <Grid.Col span={9}>
                     <Tabs radius="xs" defaultValue="gallery">
                         <Tabs.List>
-                            <Tabs.Tab value="gallery">Проекты</Tabs.Tab>
-                            <Tabs.Tab value="messages">Сертификаты</Tabs.Tab>
-                            <Tabs.Tab value="settings">Безопасность</Tabs.Tab>
+                            <Tabs.Tab value="projects">Проекты</Tabs.Tab>
+                            <Tabs.Tab value="certificates">Сертификаты</Tabs.Tab>
+                            <Tabs.Tab value="privacy">Безопасность</Tabs.Tab>
                         </Tabs.List>
 
-                        <Tabs.Panel value="gallery">Gallery tab content</Tabs.Panel>
+                        <Tabs.Panel value="projects">
+                            projects tab content
+                        </Tabs.Panel>
 
-                        <Tabs.Panel value="messages">Messages tab content</Tabs.Panel>
+                        <Tabs.Panel value="certificates">certificates tab content</Tabs.Panel>
 
-                        <Tabs.Panel value="settings">Settings tab content</Tabs.Panel>
+                        <Tabs.Panel value="privacy">privacy tab content</Tabs.Panel>
                     </Tabs>
                 </Grid.Col>
             </Grid>
