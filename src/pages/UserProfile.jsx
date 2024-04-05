40951e9917ed83643706d9fb235f23520ae98dd8
like-Ocean <my.program.is.false.2p1@gmail.com>
2024-04-05 22:04:30 +0500

    user projects

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,5 @@
 import { Layout } from '../components/ui/Layout/index.jsx';
-import { Grid, Tabs } from '@mantine/core';
+import { Card, Grid, Tabs, Image, Text } from '@mantine/core';
 import { UserInfo } from '../components/ui/UserInfo/index.jsx';
 
 export const UserProfile = () => {
@@ -19,7 +19,33 @@ export const UserProfile = () => {
                         </Tabs.List>
 
                         <Tabs.Panel value="projects">
-                            projects tab content
+                            <Grid mt={10}>
+                                <Grid.Col span={4}>
+                                    <Card
+                                        withBorder
+                                        padding="xs"
+                                        component="a"
+                                        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                                        target="_blank"
+                                    >
+                                        <Card.Section>
+                                            <Image
+                                                src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
+                                                h={200}
+                                                alt="No way!"
+                                            />
+                                        </Card.Section>
+
+                                        <Text fw={500} size="lg" mt="xs">
+                                            Название проекта
+                                        </Text>
+
+                                        <Text c="dimmed" size="sm">
+                                            описание проекта
+                                        </Text>
+                                    </Card>
+                                </Grid.Col>
+                            </Grid>
                         </Tabs.Panel>
 
                         <Tabs.Panel value="certificates">certificates tab content</Tabs.Panel>
